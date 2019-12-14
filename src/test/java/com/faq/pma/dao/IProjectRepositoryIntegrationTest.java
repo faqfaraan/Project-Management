6e738e7d19431df5b86644134fdee4d49e3a3a6b
Faraan <faqfaraan@gmail.com>
2019-12-14 09:21:37 -0500

    integration tests
    created some very basic integration tests utilizing SpringBootTest

---
+++ src/test/java/com/faq/pma/dao/IProjectRepositoryIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.faq.pma.dao;
+
+import com.faq.pma.entities.Project;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class IProjectRepositoryIntegrationTest {
+
+    @Autowired
+    IProjectRepository proRepo;
+
+    @Test
+    public void ifNewProjectSaved_thenSuccess() {
+        Project newProject = new Project("New Test Project", "COMPLETE", "Test Description");
+        proRepo.save(newProject);
+
+        assertEquals(1, proRepo.findAll().size());
+    }
+}
